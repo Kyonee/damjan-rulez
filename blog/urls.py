824d983d5a1e611ef0738030926beedfa76d702c
Kyonee <ela.fleischman@gmail.com>
2015-10-17 17:05:56 +0200

    Hello, this is my first commit, saying hello to Shakira and Damjan!

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+
+__author__ = 'Maja Ramovs'
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
